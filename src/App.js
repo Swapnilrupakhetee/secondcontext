d9c43d4f40a729b552bfa0587878558909a24fd5
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-31 22:21:23 +0545

    idk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import NoteState from './context/noteState';
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi my name is no</h1>
+      <NoteState>
+        <h1>Hi my name is no</h1>
+
+      </NoteState>
+      
     </div>
   );
 }
--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -1,1 +1,19 @@
-import { useState,useContext } from "react";
+import React from "react";
+import NoteContext from "./noteContext";
+
+const NoteState=(props) => {
+    const state ={
+        name:"swapnil",
+        section:"L1C7"
+    }
+
+
+
+    return(
+        <NoteContext.provider value={state}>
+            {props.children}
+        </NoteContext.provider>
+    )
+    
+}
+export default NoteState;
